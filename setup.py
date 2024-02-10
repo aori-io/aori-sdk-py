0f7108e73322a88e2985de3561d4b6abafbddbed
elstong <joshua@system9.io>
2024-02-10 17:17:56 -0600

    0.6

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='aori-sdk-py',
-    version='0.3',
-    packages=find_packages(),
+    version='0.6',
+    packages=['aori_sdk_py'],  # Adjusted to explicitly specify the package
+    package_dir={'aori_sdk_py': 'aori_sdk_py'},  # Map package name to directory
     install_requires=[
         'requests',
         'python-dotenv',
@@ -17,7 +18,7 @@ setup(
     author_email='joshua@aori.io',
     description='aori.io python sdk',
     long_description=long_description,
-    long_description_content_type='text/markdown',  # This is important to ensure markdown is rendered correctly on PyPI
+    long_description_content_type='text/markdown',
     keywords='aori',
     url='https://github.com/aori-io/aori-sdk-py'
 )
